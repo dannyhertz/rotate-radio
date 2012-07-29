b017925ba4e9760028411c77399ac3557a176753
Danny Hertz <me@dannyhertz.com>
2012-07-28 17:06:42 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RotateRadio::Application.config.secret_token = 'aec5522a7af0d27ac9307526d0cde5b3db9afac07944541cd41443cf424fcb615505d053570b364575f17d25a55c3522bff1da49b5e83328314dca53d90c23f9'
